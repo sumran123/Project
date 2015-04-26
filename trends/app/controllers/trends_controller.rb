e0973004124ac7cd9796ab39fc8f419f379ea99a
sumran123 <sumranrai093@gmail.com>
2015-04-27 01:40:00 +0500

    all trends table can be viewed

--- trends/app/controllers/trends_controller.rb
+++ trends/app/controllers/trends_controller.rb
@@ -1,6 +1,24 @@
 class TrendsController < ApplicationController
 	def index
-		@trend = "hello" 
+		@trends = Trend.all
+	end
+	def show
+		 
+	end
+	def new
+		 
+	end
+	def create
+		 
+	end
+	def edit
+
+	end
+	def update
+		 
+	end
+	def destroy
+		 
 	end
 	
 end

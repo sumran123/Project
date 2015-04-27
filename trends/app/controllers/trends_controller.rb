68fb5334dc6289bbe4e5187b0373cb1b6618eece
sumran123 <sumranrai093@gmail.com>
2015-04-28 03:31:37 +0500

    update and add new record

--- trends/app/controllers/trends_controller.rb
+++ trends/app/controllers/trends_controller.rb
@@ -3,7 +3,7 @@ class TrendsController < ApplicationController
 		@trends = Trend.all
 	end
 	def show
-		@trend = Trend.find(params[:id]) 
+		@trend = Trend.find(params[:id])
 	end
 	def new
 		 
@@ -14,16 +14,24 @@ class TrendsController < ApplicationController
 		  redirect_to trends_path
 	end
 	def edit
+		@trend = Trend.find params[:id]
 
 	end
 	def update
+		@trend = Trend.find params[:id]
+		@trend.update_attributes!(trend_params)
+		flash[:notice] = "#{@trend.title} was successfully updated."
+		redirect_to trends_path(@trend)
 		 
 	end
 	def destroy
 		 
 	end
+
+	private
+
 	def trend_params
-		params[:trend].permit(:title, :trendid, :price, :quantity)
+			params.require(:trend).permit(:title, :trendid, :price, :quantity)
 
 	end
 end
